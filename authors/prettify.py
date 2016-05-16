ad31fab278225c193705d6fdc024d73ad5c6c4ac
Ondřej Čertík <ondrej.certik@gmail.com>
2016-05-16 11:02:11 -0600

    Sort the list by number of patches

--- authors/prettify.py
+++ authors/prettify.py
@@ -6,6 +6,7 @@ from json import load, dumps
 from io import open
 
 author_list = load(open("authors.json"))
+author_list.sort(key=lambda x: x["np"], reverse=True)
 
 with open("authors.json", "w", encoding='utf-8') as f:
     data = dumps(author_list, ensure_ascii=False, sort_keys=True, indent=4,
