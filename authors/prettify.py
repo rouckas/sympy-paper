90045b423efcefad5f34530567efa7c2c953c6b5
Ondřej Čertík <ondrej.certik@gmail.com>
2016-05-18 10:49:10 -0600

    Add \n at the end of file

--- authors/prettify.py
+++ authors/prettify.py
@@ -11,4 +11,4 @@ author_list.sort(key=lambda x: x["sympy_commits"], reverse=True)
 with open("authors.json", "w", encoding='utf-8') as f:
     data = dumps(author_list, ensure_ascii=False, sort_keys=True, indent=4,
             separators=(',', ': '))
-    f.write(data)
+    f.write(data + "\n")
